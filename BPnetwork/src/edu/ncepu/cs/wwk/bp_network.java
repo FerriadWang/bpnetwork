4892362c956e3433b85ce4b708399c955f688b3c
Weikang Wang <wwk55551111@126.com>
2015-12-23 16:25:00 +0800

    change something not clear add setter and getter for some viriables

--- BPnetwork/src/edu/ncepu/cs/wwk/bp_network.java
+++ BPnetwork/src/edu/ncepu/cs/wwk/bp_network.java
@@ -15,58 +15,58 @@ public class bp_network extends Object {
     int inNum; //输入接点数
     int hideNum; //隐含接点数
     int outNum; //输出接点数
-
     Random R;
-    int epochs;
+    int epochs;//迭代期数
 
-    double x[]; //输入向量
-    double x1[]; //隐含接点状态值
-    double x2[]; //输出接点状态值
-
-    double o1[];
-    double o2[];
-    double w[][]; //隐含接点权值
-    double w1[][]; //输出接点权值
+    double input[]; //输入向量
+    double hidden[]; //隐含接点状态值
+    double output[]; //输出接点状态值
+    double o1[];//暂存第一层输出值
+    double o2[];//暂存第二层输出值
+    double w_ih[][]; //隐含接点权值
+    double w_ho[][]; //输出接点权值
     double rate_w; //权值学习率（输入层-隐含层)
     double rate_w1; //权值学习率 (隐含层-输出层)
     double rate_b1; //隐含层阀值学习率
     double rate_b2; //输出层阀值学习率
     double b1[]; //隐含接点阀值
     double b2[]; //输出接点阀值
-    double pp[];
-    double qq[];
+    double correct_ih[];//输入层的隐含层的调整值
+    double correct_ho[];//隐含层和输出层的调整值
     double yd[];
     double e;
     double de;
     double in_rate; //输入归一化比例系数
+    double out_rate;//输出归一化比例系数
+    
     public bp_network(int inNum, int hideNum, int outNum) {
         R = new Random();
         this.epochs = 500;
         this.inNum = inNum;
         this.hideNum = hideNum;
         this.outNum = outNum;
-        x = new double[inNum]; //输入向量
-        x1 = new double[hideNum]; //隐含接点状态值
-        x2 = new double[outNum]; //输出接点状态值
+        input = new double[inNum]; //输入向量
+        hidden = new double[hideNum]; //隐含接点状态值
+        output = new double[outNum]; //输出接点状态值
 
         o1 = new double[hideNum];
         o2 = new double[outNum];
-        w = new double[inNum][hideNum]; //隐含接点权值
-        w1 = new double[hideNum][outNum]; //输出接点权值
+        w_ih = new double[inNum][hideNum]; //隐含接点权值
+        w_ho = new double[hideNum][outNum]; //输出接点权值
         b1 = new double[hideNum]; //隐含接点阀值
         b2 = new double[outNum]; //输出接点阀值
-        pp = new double[hideNum];
-        qq = new double[outNum];
+        correct_ih = new double[hideNum];
+        correct_ho = new double[outNum];
         yd = new double[outNum];
 
         for (int i = 0; i < inNum; i++) {
             for (int j = 0; j < hideNum; j++) {
-                w[i][j] = R.nextDouble();
+                w_ih[i][j] = R.nextDouble();
             }
         }
         for (int i = 0; i < hideNum; i++) {
             for (int j = 0; j < outNum; j++) {
-                w1[i][j] = R.nextDouble();
+                w_ho[i][j] = R.nextDouble();
             }
         }
         rate_w = 0.08; //权值学习率（输入层-隐含层)
@@ -92,9 +92,11 @@ public class bp_network extends Object {
     /**********************************/
     /*****BP神经控制器算法训练函数*****/
     public void train(double p[][], double t[][],int samplenum) {
-
+    	for(int epoch=0;epoch<epochs;epoch++){
         e = 0.0;
+        //归一化
         double pmax = 0.0;
+        double tmax = 0.0;
         for (int isamp = 0; isamp < samplenum; isamp++) {
             for (int i = 0; i < inNum; i++) {
                 if (Math.abs(p[isamp][i]) > pmax) {
@@ -102,99 +104,126 @@ public class bp_network extends Object {
                 }
             }
             for (int j = 0; j < outNum; j++) {
-                if (Math.abs(t[isamp][j]) > pmax) {
-                    pmax = Math.abs(t[isamp][j]);
+                if (Math.abs(t[isamp][j]) > tmax) {
+                	tmax = Math.abs(t[isamp][j]);
                 }
             }
         } //end for isamp
         in_rate = pmax;
+        out_rate = tmax;
 
         for (int isamp = 0; isamp < samplenum; isamp++) { //循环训练一次样本
             for (int i = 0; i < inNum; i++) {
-                x[i] = p[isamp][i] / in_rate;
+                input[i] = p[isamp][i] / in_rate;
             }
             for (int i = 0; i < outNum; i++) {
-                yd[i] = t[isamp][i] / in_rate;
+                yd[i] = t[isamp][i] / out_rate;
             }
-        /*this.x=p[isamp];
+        /*this.input=p[isamp];
         this.yd=t[isamp];*/
             //构造每个样本的输入和输出标准
 
             for (int j = 0; j < hideNum; j++) {
                 o1[j] = 0.0;
                 for (int i = 0; i < inNum; i++) {
-                    o1[j] = o1[j] + w[i][j] * x[i];
+                    o1[j] = o1[j] + w_ih[i][j] * input[i];
                 }
-                x1[j] = 1.0 / (1.0 + Math.exp( -o1[j] - b1[j])); //
+                hidden[j] = 1.0 / (1.0 + Math.exp( -o1[j] - b1[j])); //
             }
 
             for (int k = 0; k < outNum; k++) {
                 o2[k] = 0.0;
                 for (int j = 0; j < hideNum; j++) {
-                    o2[k] = o2[k] + w1[j][k] * x1[j];
+                    o2[k] = o2[k] + w_ho[j][k] * hidden[j];
                 }
-                x2[k] = 1.0 / (1 + Math.exp( -o2[k] - b2[k])); //
-                //x2[k]=o2[k]-b2[k];
-                e += Math.abs(yd[k] - x2[k]) * Math.abs(yd[k] - x2[k]); //计算均方差
+                output[k] = 1.0 / (1 + Math.exp( -o2[k] - b2[k])); //
+                //output[k]=o2[k]-b2[k];
+                e += Math.abs(yd[k] - output[k]) * Math.abs(yd[k] - output[k]); //计算均方差
             }
 
             //System.out.println("ok1");
             //update hidden layer weights
             for (int k = 0; k < outNum; k++) {
-                qq[k] = (yd[k] - x2[k]) * x2[k] * (1. - x2[k]);
-                //qq[k]=(yd[k]-x2[k]);
-                //e+=Math.abs(yd[k]-x2[k])*Math.abs(yd[k]-x2[k]);//计算均方差
+                correct_ho[k] = (yd[k] - output[k]) * output[k] * (1. - output[k]);
+                //correct_ho[k]=(yd[k]-output[k]);
+                //e+=Math.abs(yd[k]-output[k])*Math.abs(yd[k]-output[k]);//计算均方差
                 for (int j = 0; j < hideNum; j++) {
-                    w1[j][k] = w1[j][k] + rate_w1 * qq[k] * x1[j];
+                    w_ho[j][k] = w_ho[j][k] + rate_w1 * correct_ho[k] * hidden[j];
                 }
             }
             //update input layer weights
             for (int j = 0; j < hideNum; j++) {
-                pp[j] = 0.0;
+                correct_ih[j] = 0.0;
                 for (int k = 0; k < outNum; k++) {
-                    pp[j] = pp[j] + qq[k] * w1[j][k];
+                    correct_ih[j] = correct_ih[j] + correct_ho[k] * w_ho[j][k];
                 }
-                pp[j] = pp[j] * x1[j] * (1. - x1[j]);
+                correct_ih[j] = correct_ih[j] * hidden[j] * (1. - hidden[j]);
                 for (int i = 0; i < inNum; i++) {
-                    w[i][j] = w[i][j] + rate_w * pp[j] * x[i];
+                    w_ih[i][j] = w_ih[i][j] + rate_w * correct_ih[j] * input[i];
                 }
             }
 
             for (int k = 0; k < outNum; k++) {
-                b2[k] = b2[k] + rate_b2 * qq[k];
+                b2[k] = b2[k] + rate_b2 * correct_ho[k];
             }
             for (int j = 0; j < hideNum; j++) {
-                b1[j] = b1[j] + rate_b1 * pp[j];
+                b1[j] = b1[j] + rate_b1 * correct_ih[j];
             }
         } //end isamp样本循环
         e = Math.sqrt(e);
-
+    	}
     } //end train
 
     /***************************************/
     /*****BP神经控制器算法模拟计算函数*****/
     public double[] sim(double psim[]) {
         for (int i = 0; i < inNum; i++) {
-            x[i] = psim[i] / in_rate;
+            input[i] = psim[i] / in_rate;
         }
         for (int j = 0; j < hideNum; j++) {
             o1[j] = 0.0;
             for (int i = 0; i < inNum; i++) {
-                o1[j] = o1[j] + w[i][j] * x[i];
+                o1[j] = o1[j] + w_ih[i][j] * input[i];
             }
-            x1[j] = 1.0 / (1. + Math.exp( -o1[j] - b1[j]));
+            hidden[j] = 1.0 / (1. + Math.exp( -o1[j] - b1[j]));
         }
         for (int k = 0; k < outNum; k++) {
             o2[k] = 0.0;
             for (int j = 0; j < hideNum; j++) {
-                o2[k] = o2[k] + w1[j][k] * x1[j];
+                o2[k] = o2[k] + w_ho[j][k] * hidden[j];
             }
-            x2[k] = 1.0 / (1.0 + Math.exp( -o2[k] - b2[k]));
-            //x2[k]=o2[k]-b2[k];
-            x2[k] = in_rate * x2[k];
+            output[k] = 1.0 / (1.0 + Math.exp( -o2[k] - b2[k]));
+            //output[k]=o2[k]-b2[k];
+            output[k] = in_rate * output[k];
         }
-        return x2;
+        return output;
     } //end sim
+    public int getInNum() {
+		return inNum;
+	}
+	public void setInNum(int inNum) {
+		this.inNum = inNum;
+	}
+	public int getHideNum() {
+		return hideNum;
+	}
+	public void setHideNum(int hideNum) {
+		this.hideNum = hideNum;
+	}
+	public int getOutNum() {
+		return outNum;
+	}
+	public void setOutNum(int outNum) {
+		this.outNum = outNum;
+	}
+	public int getEpochs() {
+		return epochs;
+	}
+	public void setEpochs(int epochs) {
+		this.epochs = epochs;
+	}
+	
 
+	
 } //end bp class
 
