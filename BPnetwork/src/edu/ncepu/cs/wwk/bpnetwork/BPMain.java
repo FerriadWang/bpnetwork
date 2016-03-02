451a5e715a61ec4f0c9d963fc8675c0c2847fdc9
weikang wang <wwk55551111@126.com>
2016-03-03 00:18:44 +0800

    slight chages for capability adjustment

--- BPnetwork/src/edu/ncepu/cs/wwk/bpnetwork/BPMain.java
+++ BPnetwork/src/edu/ncepu/cs/wwk/bpnetwork/BPMain.java
@@ -12,10 +12,9 @@ public class BPMain {
 //		bpd.readIncData("./data/data_filtered", 0, pos, 1);		
 //	}
 	
-	public long startNetwork(int basesample){
+	public long startNetwork(BPData bpd,int basesample){
 		System.out.println("BPnetwork begins...");
 		System.out.println("Start at: "+new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss-SSS").format(new Date()));
-		bpd = new BPData();
 		System.out.println("Reading data...");
 		long pos  = bpd.readBaseData("./data/data_filtered",0, basesample);
 		if(pos !=0){
@@ -23,8 +22,8 @@ public class BPMain {
 			double t [][] = bpd.getTData();
 			System.out.println("Data read!");
 			System.out.println("Start training...");
-			BPConfig bpc = new BPConfig(p[0].length, 6, t[0].length,500);
-			BPNetwork bpn = new BPNetwork(bpc);
+			bpc = new BPConfig(p[0].length, 6, t[0].length,500);
+			bpn = new BPNetwork(bpc);
 			bpn.train(p, t, basesample);
 		}else{
 			System.out.println("error: Error code+ "+ pos);
@@ -33,5 +32,36 @@ public class BPMain {
 		return pos;
 	}
 	
-	private BPData bpd;
+	public long incLearning(BPData bpd, long pos, int lines){
+		System.out.println("inLearning begins...");
+		System.out.println("Start at: "+new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss-SSS").format(new Date()));
+		System.out.println("Reading data...");
+		long returnpos = bpd.readIncData("./data/data_filtered", 0, pos, 1);
+		if(returnpos !=0){
+			double p [][] = bpd.getpIncData();
+			double t [][] = bpd.gettIncData();
+			System.out.println("incData read!");
+			System.out.println("Start training...");
+			bpc = new BPConfig(p[0].length, 6, t[0].length,500);
+			bpn = new BPNetwork(bpc);
+			bpn.train(p, t, lines);
+		}else{
+			System.out.println("error: Error code+ "+ pos);
+		}
+		System.out.println("End at: "+new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss-SSS").format(new Date()));
+		return pos;
+	}
+	
+	public BPConfig getBpc() {
+		return bpc;
+	}
+	public BPNetwork getBpn() {
+		return bpn;
+	}
+//	public BPData getBpd() {
+//		return bpd;
+//	}
+	private BPConfig bpc;
+	private BPNetwork bpn;
+//	private BPData bpd;
 }
